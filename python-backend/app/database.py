3316900f4af9ad410daa1b17f560573c9809bebe
Aiqi Shuai <mk81472k9@gmail.com>
2026-05-21 10:53:33 +0200

    refactor: 删除未使用的 SessionLocal

--- python-backend/app/database.py
+++ python-backend/app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from databases import Database
 
 from app.config import settings
@@ -11,8 +10,6 @@ engine = create_engine(
     pool_recycle=3600,
     echo=False,
 )
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
